292cd8d127b34511a93e8fc098ce0f77ddc74e86
DevSergioGC <sergiocamachog12@gmail.com>
2023-11-07 00:28:32 -0400

    first attempt

--- exercises/exercise_2.js
+++ exercises/exercise_2.js
@@ -1,1 +1,17 @@
 // Te doy un arreglo de strings y me devuelves el arreglo pero removiendo cualquier valor duplicado
+
+let input = [
+  "John",
+  "Mike",
+  "Danny",
+  "Lisa",
+  "Sophie",
+  "John",
+  "Mike",
+  "Danny",
+  "Lisa",
+  "Sophie",
+];
+
+fixInput = input.filter((value, index) => input.indexOf(value) === index);
+console.log(fixInput);
