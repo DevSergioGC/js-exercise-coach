27e9c55d5c2a0b1c343c023e0f183db0bfcd2392
DevSergioGC <sergiocamachog12@gmail.com>
2023-11-06 21:18:57 -0400

    first attempt
    when i print the results, i get a comma "," after every person's age

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -1,9 +1,16 @@
 let input = [
-    { name: "John", age: 21, city: "New York" },
-    { name: "Mike", age: 28, city: "Moscow" },
-    { name: "Danny", age: 30, city: "London" },
-    { name: "Lisa", age: 26, city: "Paris" },
-    { name: "Sophie", age: 19, city: "Berlin" },
-  ];
-  
+  { name: "John", age: 21, city: "New York" },
+  { name: "Mike", age: 28, city: "Moscow" },
+  { name: "Danny", age: 30, city: "London" },
+  { name: "Lisa", age: 26, city: "Paris" },
+  { name: "Sophie", age: 19, city: "Berlin" },
+];
+
 //   Quiero las personas que tienen menos de 25 años y la cantidad que son
+
+fixInput = input.filter((person) => person.age < 25);
+console.log(
+  `\nCantidad de personas menores a 25 años: ${
+    fixInput.length
+  }\n\n${fixInput.map((person) => (`${person.name} --> ${person.age} años\n`))}`
+);
